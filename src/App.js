8f4e7897a984f1fc3aa454a450635a1d7693820d
Antti Luukka <antti.luukka@gmail.com>
2022-12-08 22:49:14 +0200

    Fix errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // Import dependencies
-import React, { useRef, useState, useEffect } from "react";
-import * as tf from "@tensorflow/tfjs";
+import React, { useRef, useEffect } from "react";
+// import * as tf from "@tensorflow/tfjs";
 import * as cocossd from "@tensorflow-models/coco-ssd";
 // import * as tfmodel from "@tensorflow-models/tfmodel";
 import Webcam from "react-webcam";
@@ -52,7 +52,7 @@ function App() {
     }
   };
 
-  useEffect(()=>{runCoco()},[]);
+  useEffect(()=>{runCoco()},[runCoco]);
 
   return (
     <div className="App">
